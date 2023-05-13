d19ef244dc42e60b01bfa78b19f21a81a5935668
Andrey C. Dos Santos <Andrey-Carlos@outlook.com>
2023-05-12 21:10:50 -0300

    Criado aplicação

---
+++ Readme.md
@@ -0,0 +1,33 @@
+# Drey Downloader
+
+Este é um aplicativo de download de vídeos do YouTube com opção de conversão para AVI. Ele utiliza as bibliotecas `os`, `tkinter`, `pytube` e `moviepy` para realizar o download e a conversão dos vídeos.
+
+## Requisitos
+
+Certifique-se de ter as seguintes bibliotecas instaladas:
+
+- `tkinter`
+- `pytube`
+- `moviepy`
+
+Você pode instalá-las executando o seguinte comando:
+
+pip install tkinter pytube moviepy
+
+## Como executar
+
+Para executar o aplicativo, execute o seguinte comando:
+
+python app.py
+
+## Uso
+
+1. Digite os links dos vídeos que deseja baixar na caixa de texto "Links para download".
+2. Selecione a extensão desejada no menu suspenso "Formato do arquivo".
+3. Clique no botão "Baixar" para iniciar o download e, se necessário, a conversão para AVI.
+4. O status do download será exibido na parte inferior da janela.
+
+## Observações
+
+- Certifique-se de ter uma conexão com a internet para que o download seja realizado com sucesso.
+- Os arquivos baixados serão salvos no diretório onde o aplicativo está sendo executado.
---
+++ app.py
@@ -0,0 +1,62 @@
+import os
+import tkinter as tk
+from pytube import YouTube
+from moviepy.editor import VideoFileClip
+
+def download_files(urls, extension):
+    if urls != "":
+        yt = YouTube(urls) 
+        status_text.set("Download Iniciado!")
+        window.update()
+
+        if extension == "avi":
+            stream = yt.streams.filter(progressive=True, file_extension="mp4").order_by('resolution').desc().first()
+            video_path = stream.download()
+                 
+            # Converter para AVI usando moviepy
+            output_path = video_path.replace(".mp4", ".avi")
+            clip = VideoFileClip(video_path)
+            status_text.set("Converção Iniciada!")
+            window.update()
+            clip.write_videofile(output_path, codec='libxvid')
+
+            # Remover o arquivo de vídeo original
+            clip.close()
+            os.remove(video_path)
+        else:
+            yt.streams.filter(progressive=True, file_extension=extension).order_by('resolution').desc().first().download()
+
+        status_text.set("Download concluído!")
+
+    
+if __name__ == "__main__":
+    window = tk.Tk()
+    window.title("Drey Downloader")
+    window.geometry("300x200")
+
+    # Adiciona uma coluna extra vazia no início e no final da janela
+    window.grid_columnconfigure(0, weight=1)
+    window.grid_columnconfigure(4, weight=1)
+
+    label_urls = tk.Label(window, text="Links para download")
+    label_urls.grid(row=0, column=2, sticky="ew", pady=(20, 5))
+
+    field_urls = tk.Entry(window)
+    field_urls.grid(row=1, column=2, sticky="ew")
+
+    selected_option = tk.StringVar(window)
+    selected_option.set("mp4")  # Define a opção padrão selecionada
+
+    file_extension = tk.OptionMenu(window, selected_option, "mp4", "avi", "mp3")
+    file_extension.grid(row=3, column=2, sticky="ew", pady=10)
+
+    button_download = tk.Button(window, text="Baixar", command=lambda: download_files(field_urls.get(), file_extension.get()))
+    button_download.grid(row=4, column=2, sticky="ew", pady=(0, 10))
+
+    status_text = tk.StringVar()
+    status_text.set("...")
+
+    status_label = tk.Label(window, textvariable=status_text)
+    status_label.grid(row=5, column=2, sticky="n", pady=(0, 0))
+
+    window.mainloop()
